1dc6e5b9ba1b84a85e10e1be1c55ea5c97768059
zxj <zhuxiujia@qq.com>
2026-04-25 22:36:49 +0800

    add example

--- example/src/mssql.rs
+++ example/src/mssql.rs
@@ -1,16 +1,18 @@
+use rbdc::Error;
 use rbdc::pool::Pool;
 use rbdc_mssql::MssqlDriver;
 use rbdc_pool_fast::FastPool;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     //let uri = "jdbc:sqlserver://localhost:1433;User=SA;Password={TestPass!123456};Database=master;";
     //let uri = "sqlserver://SA:TestPass!123456@localhost:1433/master";
     //let uri = "Server=localhost,1433;User Id=SA;Password=TestPass!123456;Database=master;";
     let uri = "mssql://SA:TestPass!123456@localhost:1433/master";
-    let pool = FastPool::new_url(MssqlDriver {}, uri).unwrap();
-    let mut conn = pool.get().await.unwrap();
-    let v = conn.exec_decode("SELECT 1", vec![]).await.unwrap();
+    let pool = FastPool::new_url(MssqlDriver {}, uri)?;
+    let mut conn = pool.get().await?;
+    let v = conn.exec_decode("SELECT DB_NAME() AS CurrentDatabase", vec![]).await?;
     println!("{}", v);
     //if need decode use `let result:Vec<Table> = rbs::from_value(v)?;`
+    Ok(())
 }
